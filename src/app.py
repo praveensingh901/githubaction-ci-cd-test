8ce85c18ded92a2ca82b17d9ff0c9cca98c1a5ad
praveen <praveen@example.com>
2025-06-06 16:30:48 +0400

    another commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def system_info():
     hostname = socket.gethostname()
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return jsonify({'hostname': hostname, 'current_time': current_time, 'message' : 'How are you doing today !!'})
+    return jsonify({'hostname': hostname, 'current_time': current_time, 'message' : 'How are you doing today !!!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
